84d5150d31d8afe15fd13ae8e3cd6766dba7aeac
waridrox <mohdwarid4@gmail.com>
2021-11-03 15:33:53 +0530

    Checking for file size from response header

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	// "bufio"
+	"errors"
 	"fmt"
-	"net/http"
-
-	// "go/format"
 	"log"
+	"net/http"
+	"strconv"
 	"time"
-	// "go/format"
-	// "os"
 )
 
 type Download struct {
@@ -26,8 +23,8 @@ type Download struct {
 func main() {
 	startTime := time.Now()
 	d := Download{
-		Url:           "https://www.dropbox.com/s/f63i7s11ydm2cu6/542.Carousel1-potassium-1024w-1366h%402x~ipad.jpg?dl=0",
-		TargetPath:    "ipadwallpaper.jpg",
+		Url:           "https://www.dropbox.com/s/f63i7s11ydm2cu6/542.Carousel1-potassium-1024w-1366h%402x~ipad.jpg?dl=1",
+		TargetPath:    "wallpaper.jpg",
 		TotalSections: 10,
 	}
 
@@ -36,7 +33,7 @@ func main() {
 		log.Fatalf("Error occured while downloading the file: %s\n", err)
 	}
 	fmt.Printf("Download completed in %v seconds\n", time.Now().Sub(startTime).Seconds())
-	fmt.Printf("Welcome to the Download Manager")
+	fmt.Printf("Welcome to the Download Manager\n")
 }
 
 func (d Download) Do() error {
@@ -50,8 +47,17 @@ func (d Download) Do() error {
 	if err != nil {
 		return err
 	}
-	fmt.Printf("Status %v", resp.StatusCode)
+	fmt.Printf("Status %v\n", resp.StatusCode)
 
+	if resp.StatusCode > 299 {
+		return errors.New(fmt.Sprintf("Can't process! Response status is: %v", resp.StatusCode))
+	}
+
+	size, err := strconv.Atoi(resp.Header.Get("Content-Length"))
+	if err != nil {
+		return err
+	}
+	fmt.Printf("File size: %v bytes\n", size)
 	return nil
 }
 
